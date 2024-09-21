0cf298610ba0ddec549d1aad86cf3d111d2ea1ad
Aleksandar Efremov <efremov.alex@yahoo.bg>
2024-09-21 12:54:52 +0300

    21.09.2024

--- django_urls_and_views/department/urls.py
+++ django_urls_and_views/department/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from django_urls_and_views.department import views
 
 urlpatterns = [
-    path("department/", views.index)
+    path("<variable>/", views.view_with_name),
+    path("department/", views.index),
+    path("<slug:slug>/", views.view_with_slug),
+
+    re_path(r'^archive/(?P<archive_year>202[0-3])/$', views.show_archive),
 ]
